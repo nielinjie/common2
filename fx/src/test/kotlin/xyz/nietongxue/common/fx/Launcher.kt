f9e505ccd18b6dbdc3783dccd84993f10ca28775
nielinjie <nielinjie@gmail.com>
2026-06-03 15:42:30 +0800

    refactor(fx): 移除未使用的导入
    - 删除了 HelloApplication 的未使用导入声明
    - 保持了原有的 JavaFX 应用启动功能

--- fx/src/test/kotlin/xyz/nietongxue/common/fx/Launcher.kt
+++ fx/src/test/kotlin/xyz/nietongxue/common/fx/Launcher.kt
@@ -1,7 +1,6 @@
 package xyz.nietongxue.common.fx
 
 import javafx.application.Application
-import xyz.nietongxue.common.fx.HelloApplication
 
 fun main() {
     Application.launch(HelloApplication::class.java)
